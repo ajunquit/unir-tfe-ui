118853ddd8befc8f058f6b1a14d80ccb795f24e4
Alejandro Junqui <ajunquit@gmail.com>
2025-09-17 10:09:50 -0500

    add MonitoringController

---
+++ src/UNIR.TFE.Polyrepo.UI/Controllers/MonitoringController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UNIR.TFE.Polyrepo.UI.Controllers
+{
+    public class MonitoringController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet("/health")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Health() => Content("OK", "text/plain");
+    }
+}
